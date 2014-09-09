487bed768049c6a4561adb7b5e6b18a02fabdc59
mhcamillow <mhcamillow@hotmail.com>
2014-09-09 17:00:39 -0300

    Fuçando

--- src/ngram.py
+++ src/ngram.py
@@ -6,31 +6,25 @@ import re, collections
 class NLP():
 
   def __init__(self):
-    self.texto = self.normalize_text(file('big.txt').read())
-    self.lista = re.findall(r'[a-zA-Z]+', self.texto)
+    self.text = self.normalize_text(file('big.txt').read())
+    self.known_words = re.findall(r'[a-zA-Z]+', self.text)
 
   def ngram(self, previous_words):
     possible_words = collections.defaultdict(lambda: 0)
 
-    for i, known_word in enumerate(self.lista):
+    for i, known_word in enumerate(self.known_words):
       for x, previous_word in enumerate(previous_words):
-        if (previous_word.lower() != self.lista[i + x]):
+        if (previous_word.lower() != self.known_words[i + x]):
           break;
       else:
-        possible_words[self.lista[i + len(previous_words)]] += 1
+        possible_words[self.known_words[i + len(previous_words)]] += 1
 
     return possible_words
 
-  def train(self, lista):
-    r = collections.defaultdict(lambda: 0)
-    for word in lista:
-      r[word] += 1
-    return r
-
-  def normalize_text(self, texto):
+  def normalize_text(self, text):
     codif = 'utf-8'
-    return normalize('NFKD', texto.decode(codif)).encode('ASCII', 'ignore').lower()
+    return normalize('NFKD', text.decode(codif)).encode('ASCII', 'ignore').lower()
 
 #Example:
 a = NLP()
-print(dict(a.ngram(['you'])))
+print(dict(a.ngram(['the', 'king'])))
