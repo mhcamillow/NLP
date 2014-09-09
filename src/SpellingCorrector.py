3035e159d01abb2b76bbe9e031d6c6a2193c9643
mhcamillow <mhcamillow@hotmail.com>
2014-09-09 11:57:36 -0300

    alterações

--- src/SpellingCorrector.py
+++ src/SpellingCorrector.py
@@ -2,38 +2,35 @@ import re, collections
 
 class SpellingCorrector(object):
   
-  def words(text):
+  def __init__(self):
+    self.NWORDS = self.train(self.words(file('big.txt').read()))
+    self.alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+  def words(self, text):
     return re.findall('[a-z]+', text.lower())
 
-  def train(features):
-
+  def train(self, features):
     model = collections.defaultdict(lambda: 1)
     for f in features:
       model[f] += 1
-
     return model
 
-  NWORDS = train(words(file('big.txt').read()))
-
-  alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-  def edits1(word):
+  def edits1(self, word):
     splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
     deletes    = [a + b[1:] for a, b in splits if b]
     transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b) > 1]
-    replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
-    inserts    = [a + c + b     for a, b in splits for c in alphabet]
+    replaces   = [a + c + b[1:] for a, b in splits for c in self.alphabet if b]
+    inserts    = [a + c + b     for a, b in splits for c in self.alphabet]
     return set(deletes + transposes + replaces + inserts)
 
-  def known_edits2(word):
-    return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
+  def known_edits2(self, word):
+    return set(e2 for e1 in self.edits1(word) for e2 in self.edits1(e1) if e2 in self.NWORDS)
 
-  def known(words): return set(w for w in words if w in NWORDS)
+  def known(self, words): return set(w for w in words if w in self.NWORDS)
 
-  def correct(word):
-    candidates = known([word]) or known(edits1(word)) or known_edits2(word) or [word]
-    return max(candidates, key=NWORDS.get)
+  def correct(self, word):
+    candidates = self.known([word]) or self.known(self.edits1(word)) or self.known_edits2(word) or [word]
+    return max(candidates, key=self.NWORDS.get)
 
-#Exemplo:
-#a = SpellingCorrector
-#print(a.correct("monstros"))
+a = SpellingCorrector()
+print(a.correct('monstros'))
